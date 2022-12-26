05ff12ba0d238cf906c9e9a09ff52e70ebd0f602
Sumit3916 <targetmsar@gmail.com>
2022-12-26 18:12:51 +0530

    This is my First Project

---
+++ myapp/kisraon/shopify/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+def indexPage(request):
+	return render(request,'index.html')
+
+'''def loginPage(request):
+	return render(request,'pages/login.html')'''
+
+'''def addtocart(request):
+	return render(request,'pages/addtocart.html')'''
+
+'''def addressPage(request):
+		return render(request,'pages/address.html')'''	
+
+'''def orderPage(request):
+	return render(request,'pages/order.html')	'''
+
+'''def checkoutPage(request):
+	return render(request,'pages/checkout.html')	'''
+
+'''def productDeatil(request):
+	return render(request,'pages/productdeatils.html')'''
+
+'''def profilePage(request):
+	return render(request,'pages/profile.html')	'''
+
+'''def mobilePage(request):
+	return render(request,'index.html')	
+'''
+'''def customerRegistartionPage(request):
+				return render(request,'pages/customerregistration.html')'''			
